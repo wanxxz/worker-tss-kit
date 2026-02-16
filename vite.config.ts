b4a5a537edd31798e27993b510db38f949173e9e
Yiwan Zhou <mail@yiwan.me>
2026-02-16 09:11:53 +0000

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
@@ -11,6 +12,7 @@ export default defineConfig({
         virtualRouteConfig: './src/routes.ts'
       }
     }),
-    viteReact()
+    viteReact(),
+    vanillaExtractPlugin()
   ]
 })
